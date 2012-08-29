7ac5f265c523bcb0ecf3fc3b548851e617656571
Andrew Lim <andrew.b.lim@gmail.com>
2012-08-29 00:59:44 -0400

    Now generates an image, seems to work at least for square grids

--- gridify.py
+++ gridify.py
@@ -1,5 +1,6 @@
 
 import argparse
+import sys
 from imagegridder import generateGrid
 
 if __name__ == "__main__":
@@ -14,31 +15,38 @@ if __name__ == "__main__":
                                      epilog=help_text)
     
     parser.add_argument("output", 
-                        metavar="output", type=str, nargs=1,
+                        metavar="output", type=str,
                         help="the filename to write the output to")
     parser.add_argument("-i", "--image",  
-                        metavar="file", type=str, nargs="+",
+                        metavar="file", type=str, nargs="+", default=[],
                         help="a list of input image files")
     parser.add_argument("-s", "--size", 
                         metavar=("W", "H"), type=int, nargs=2, default=(None, None),
                         help="the desired width W and height H of the image grid")
     parser.add_argument("-r", "--rows", 
-                        metavar="R", type=int, nargs=1, default=None,
+                        metavar="R", type=int, default=None,
                         help="the number of rows in the grid")
     parser.add_argument("-c", "--cols", 
-                        metavar="C", type=int, nargs=1, default=None,
+                        metavar="C", type=int, default=None,
                         help="the number of columns in the grid")
     parser.add_argument("-b", "--border", 
-                        metavar="W", type=int, nargs=1, default=0,
+                        metavar="W", type=int, default=0,
                         help="the width W of the gridlines (default 0)")
     parser.add_argument("-l", "--labels",
                         action="store_true",
                         help="generate row and column labels (will cause image to be bigger than specified size)")
+    parser.add_argument("-p", "--permute",
+                        action="store_true",
+                        help="randomly shuffle the order of input image files")
     parser.add_argument("-v", "--verbose",
                         action="store_true",
                         help="verbose mode")
     
     args = parser.parse_args()
+    
+    if (len(args.image) == 0):
+        print "No images specified, nothing done (use the -i flag to specify them)."
+        sys.exit(0)
     
     generateGrid(input_images=args.image, 
                  output_image=args.output,
@@ -48,6 +56,6 @@ if __name__ == "__main__":
                  cols=args.cols,
                  border_width=args.border,
                  add_labels=args.labels,
+                 permute=args.permute,
                  verbose=args.verbose)
-    
     
--- imagegridder.py
+++ imagegridder.py
@@ -1,24 +1,90 @@
 
 import Image
+import math
+import random
+import sys
 
 ASPECT_TOLERANCE = 0
+SCALEUP_LINEAR_TOLERANCE = 1.05
 
 def generateGrid(input_images, output_image, height=None, width=None, 
                  rows=None, cols=None, border_width=0, 
-                 add_labels=False, verbose=False):
+                 add_labels=False, permute=False, verbose=False):
     
-    if len(input_images) == 0:
-        print "No images specified"
-        exit()
+    if permute: random.shuffle(input_images)
     
-    aspect_ratios = []
-    if verbose: print "Aspect ratios:"
+    images = []
     for input_image in input_images:
-        im = Image.open(input_image)
-        aspect_ratios.append(im.size[0] / im.size[1])
-        if verbose: print "%s %0.4f" % (input_image, im.size[0] / im.size[1])
+        image = Image.open(input_image)
+        images.append(image)
+        if verbose:
+            print "%s %s %s (aspect %0.3f)" % \
+                  (input_image, image.size, image.mode, image.size[0]/image.size[1])
     
+    # check aspect ratios for similarity
+    
+    aspect_ratios = [image.size[0] / image.size[1] for image in images]
     if max(aspect_ratios) - min(aspect_ratios) > ASPECT_TOLERANCE:
         print "Warning: max - min aspect ratio > %f, some images may be distorted." % ASPECT_TOLERANCE
     
+    # default values/error checks on the number of rows and cols
     
+    n = float(len(input_images))
+    if rows == None and cols == None:
+        if int(math.sqrt(n)) != math.sqrt(n):
+            raise Exception("Rows/columns unspecified, square grid presumed but # of images %d is not a perfect square" % n)
+        else:
+            rows = int(math.sqrt(n))
+            cols = rows
+    elif rows == None:
+        if int(n/rows) != (n/rows):
+            raise Exception("Rows unspecified, columns %d do not divide # of images %d" % (cols, n))
+        else:
+            rows = int(n/cols)
+    elif cols == None:
+        if int(n/rows) != n/rows:
+            raise Exception("Columns unspecified, rows %d do not divide # of images %d" % (rows, n))
+        else:
+            cols = int(n/rows)
+    if rows * cols != n:
+        raise Exception("Rows %d * columns %d do not add up to # of images %d" % (rows, cols, n))
+    
+    # default values/error checks on the image size
+    
+    if height == None or width == None:
+        sizes = [image.size[0] * image.size[1] for image in images]
+        smallest_image = images[sizes.index(min(sizes))]
+        (width, height) = smallest_image.size
+    
+    for image in images:
+        if width > image.size[0] * SCALEUP_LINEAR_TOLERANCE:
+            print "Warning: image width will be scaled beyond tolerance max %f" % SCALEUP_LINEAR_TOLERANCE
+        if height > image.size[1] * SCALEUP_LINEAR_TOLERANCE:
+            print "Warning: image height will be scaled beyond tolerance max %f" % SCALEUP_LINEAR_TOLERANCE
+    
+    if rows > height:
+        raise Exception("Number of rows %d greater than height of image %d" % (rows, height))
+    if cols > width:
+        raise Exception("Number of columns %d greater than width of image %d" % (cols, width))
+    
+    # generate new image
+    
+    # to do: inherit mode from child images? for now just RGB hard-coded
+    output = Image.new("RGB", size=(width, height))
+    box_size = (float(width)/cols, float(height)/rows)
+    
+    for r in range(rows):
+        for c in range(cols):
+            box = (int(box_size[0] * r), int(box_size[1] * c), 
+                   int(box_size[0] * (r+1)), int(box_size[1] * (c+1)))
+            print box
+            i = cols * r + c
+            images[i] = images[i].resize((width, height))
+            images[i].save(str(i) + ".jpg")
+            output.paste(images[i].crop(box), box)
+    
+    # if add_labels == True, add some labels on (increasing image size)
+    
+    
+    
+    output.save(output_image)
